0f918a106698319b6520f2441deccb694eb5b882
Nagapurnasaiguntu <124567535+NagaPurnaSai@users.noreply.github.com>
2025-03-05 16:47:04 +0530

    Update auth.js

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -4,8 +4,8 @@ export default async function handler(req, res) {
   const { email, password } = req.body;
 
   const { data, error } = await supabase.auth.signInWithPassword({
-    email,
-    password
+  email: "sro0618346@gmail.com",
+  password: "917727sAi"
   });
 
   if (error) return res.status(400).json({ error: error.message });
